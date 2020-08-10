abc7af55a9e0df699e0fd1a0d016afe888ba4bf3
jagarzone6 <jagarzone@gmail.com>
2020-08-09 23:29:07 -0500

    lint fix

--- support/pages.py
+++ support/pages.py
@@ -2,8 +2,9 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from pages.login_page import LoginPage
 from pages.welcome_page import WelcomePage
 
-login:LoginPage = None
-welcome:WelcomePage = None
+login: LoginPage = None
+welcome: WelcomePage = None
+
 
 def with_driver(driver: WebDriver):
     global login, welcome
